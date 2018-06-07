66b8ddbead7ceebd4db29c7e64cdcce41672ded8
fNedopetalski <37910959+fNedopetalski@users.noreply.github.com>
2018-06-06 21:02:42 -0300

    NÃO TA PRONTO

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject pause;
+
+    private bool mostrarPause = false;
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Invoke("aparecePause", 0.3f);
+        }
+
+        else if (mostrarPause == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Voltar();
+        }
+    }
+
+    void aparecePause()
+    {
+        if(!mostrarPause)
+        {
+            pause.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    void Voltar()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+    }
 }
