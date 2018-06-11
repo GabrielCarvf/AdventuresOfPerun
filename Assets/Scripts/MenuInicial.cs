ad39cd961b050399558e00890e573258afd10481
Gabriel <gabrielcarvalho33527@gmail.com>
2018-06-11 14:35:24 -0300

    Creditos

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -3,8 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class MenuInicial : MonoBehaviour {
 
+    Animator animator;
+    public GameObject telaCreditos;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            FecharCreditos();
+        }
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }	
+    }
+    public void MostrarCreditos()
+    {
+        animator.SetBool("AbrirCreditos", true);
+        animator.SetBool("FecharCreditos", false);
+    }
+    public void FecharCreditos()
+    {
+        animator.SetBool("FecharCreditos", true);
+        animator.SetBool("AbrirCreditos", false);
+    }
 }
